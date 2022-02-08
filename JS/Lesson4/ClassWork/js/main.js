41917c164c40da02e88c7e90bf966d6686b8d8c5
NABius <nabius@ukr.net>
2022-02-08 13:16:05 +0200

    add lesson4 class & home

---
+++ JS/Lesson4/ClassWork/js/main.js
@@ -0,0 +1,140 @@
+//TODO створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+console.log("створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)")
+
+function findMin(a, b, c) {
+    let result = a;
+    if (result > b) {
+        result = b;
+    } else if (result > c) {
+        result = c;
+    }
+    return result;
+}
+
+console.log(`Min = ${findMin(546, 6767, 34)}`)
+
+//TODO - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+console.log("створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)")
+
+function findMax(a, b, c) {
+    let result = a;
+    if (result < b) {
+        result = b;
+    } else if (result < c) {
+        result = c;
+    }
+    return result;
+}
+
+console.log(`Max = ${findMax(546, 6767, 34)}`)
+
+//TODO - створити функцію яка повертає найбільше число з масиву
+console.log("створити функцію яка повертає найбільше число з масиву")
+
+function findMaxInArray(array) {
+    let result = array[0];
+    for (let arrayElement of array) {
+        if (result < arrayElement) {
+            result = arrayElement;
+        }
+    }
+    return result;
+}
+
+console.log(`Max = ${findMaxInArray([546, 6767, 34])}`)
+
+//TODO - створити функцію яка повертає найменьше число з масиву
+console.log("створити функцію яка повертає найменьше число з масиву")
+
+function findMinInArray(array) {
+    let result = array[0];
+    for (let arrayElement of array) {
+        if (result > arrayElement) {
+            result = arrayElement;
+        }
+    }
+    return result;
+}
+
+console.log(`Min = ${findMinInArray([546, 6767, 34])}`)
+
+//TODO - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+console.log("створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його")
+
+function sumOfArray(array) {
+    let result = 0;
+    for (let arrayElement of array) {
+        result += arrayElement
+    }
+    return result;
+}
+
+console.log(`Sum = ${sumOfArray([30, 20, 50])}`)
+
+//TODO- створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+console.log("створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його")
+
+function averageOfArray(array) {
+    let result = 0;
+    for (let arrayElement of array) {
+        result += arrayElement
+    }
+    return result / array.length;
+}
+
+console.log(`Average = ${averageOfArray([30, 20, 50, 100])}`)
+
+//TODO - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+console.log("створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше")
+
+function minNumber(...array) {
+    let min = array[0], max = array[0];
+    for (let arrayElement of array) {
+        if (min > arrayElement) {
+            min = arrayElement;
+        }
+        if (max < arrayElement) {
+            max = arrayElement;
+        }
+    }
+    console.log(`Max = ${max}`);
+    return min;
+}
+
+console.log(`Min = ${minNumber(30, 20, 50, 100)}`)
+
+//TODO- створити функцію яка заповнює масив рандомними числами (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+console.log("створити функцію яка заповнює масив рандомними числами та виводить його.")
+function fillArray(array) {
+    for (let arrayElement of array) {
+        arrayElement = Math.round(Math.random() * 100);
+        console.log(arrayElement);
+    }
+}
+
+fillArray([null, null, null, null, null, null, null]);
+
+//TODO - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+console.log("створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit")
+
+function fillArrayWithLimit(array, limit) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = Math.round(Math.random() * limit);
+    }
+    return array;
+}
+
+console.log(fillArrayWithLimit([100, 101, 102, 103, 104, 105, 106], 50))
+
+//TODO - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+console.log("Функція приймає масив та робить з нього новий масив в зворотньому порядку.")
+
+function reverseArray(array) {
+    let reverseArr = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        reverseArr[reverseArr.length] = array[i];
+    }
+    return reverseArr;
+}
+
+console.log(reverseArray([30, 20, 50, 100]));
---
+++ JS/Lesson4/HomeWork/js/main.js
@@ -0,0 +1,99 @@
+//TODO - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+console.log("створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б");
+
+function areaRectangle(a, b) {
+    return a * b;
+}
+
+console.log(`Площа прямокутника: ${areaRectangle(5, 3)}`);
+
+//TODO - створити функцію яка обчислює та повертає площу кола з радіусом r
+console.log("створити функцію яка обчислює та повертає площу кола з радіусом r");
+
+function areaCircle(r) {
+    return Math.PI * Math.pow(r, 2);
+}
+
+console.log(`Площа кола: ${areaCircle(5)}`);
+
+//TODO - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіусом r
+console.log("створити функцію яка обчислює та повертає площу циліндру висотою h, та радіусом r");
+
+function areaCylinder(r, h) {
+    return Math.PI * Math.pow(r, 2) * h;
+}
+
+console.log(`Площа циліндра: ${areaCylinder(5, 10)}`);
+
+//TODO - створити функцію яка приймає масив та виводить кожен його елемент
+console.log("створити функцію яка приймає масив та виводить кожен його елемент")
+
+function arrayOut(array) {
+    for (let arrayElement of array) {
+        console.log(arrayElement);
+    }
+}
+
+arrayOut(["teyty", 4543, 65445, true])
+
+//TODO - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+document.write("створити функцію яка створює параграф з текстом. Текст задати через аргумент")
+
+function createParagraph(text) {
+    document.write(`<p>${text}</p>`);
+}
+
+createParagraph("Параграф");
+
+//TODO - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+document.write("створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий")
+
+function create3Li(text) {
+    document.write("<ul>");
+    document.write(`<li>${text}</li>`);
+    document.write(`<li>${text}</li>`);
+    document.write(`<li>${text}</li>`);
+    document.write("</ul>");
+}
+
+create3Li("Список")
+//TODO - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+document.write("створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим");
+
+function createNLi(text, count) {
+    document.write("<ul>");
+    for (let i = 0; i < count; i++) {
+        document.write(`<li>${text} ${i + 1}</li>`);
+    }
+    document.write("</ul>");
+}
+
+createNLi("List", 5)
+
+//TODO - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+document.write("створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список")
+
+function createLiFromArray(...array) {
+    document.write("<ul>");
+    for (let arrayElement of array) {
+        document.write(`<li>${arrayElement}</li>`);
+    }
+    document.write("</ul>");
+}
+
+createLiFromArray("List", 5, false)
+
+//TODO - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+document.write("створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.")
+
+function createDivFromArrObject(array) {
+    for (let arrayElement of array) {
+        document.write("<div>");
+        for (let personItem in arrayElement) {
+            document.write(`<div>${personItem}: ${arrayElement[personItem]}</div>`);
+        }
+        document.write("</div>");
+    }
+}
+
+createDivFromArrObject([{id: 0, name: "vasya", age: 30}, {id: 1, name: "petya", age: 31}])
