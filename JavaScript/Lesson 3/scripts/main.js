bfcdb33bb25d67b430be52130a54e8d85bde783c
NABius <nabius@ukr.net>
2023-11-28 18:45:26 +0200

    changed condition status in lesson 3 js

--- JavaScript/Lesson 3/scripts/main.js
+++ JavaScript/Lesson 3/scripts/main.js
@@ -111,14 +111,14 @@ let users = [
 // за допомоги циклу вивести:
 //     - користувачів зі статусом true
 for (const user of users) {
-    if (user["status"] === true) {
+    if (user["status"] ) {
         document.writeln(`<div>user:${user["name"]} with status ${user["status"]}</div>`)
     }
 }
 
 // - користувачів зі статусом false
 for (const user of users) {
-    if (user["status"] === false) {
+    if (!user["status"]) {
         document.writeln(`<div>user:${user["name"]} with status ${user["status"]}</div>`)
     }
 }
